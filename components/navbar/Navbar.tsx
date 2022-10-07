cdb1c051b07f7eb24c335e9cb61c60181ccda990
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2022-10-07 16:48:15 -0700

    first commit

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+
+interface NavbarProps {
+    
+}
+ 
+const Navbar: FunctionComponent<NavbarProps> = () => {
+    return (
+        <nav>
+            
+        </nav>
+    );
+}
+ 
+export default Navbar;
